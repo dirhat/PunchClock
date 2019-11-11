41809cc93f283ff95cddb2bd3c4f5f13e7b06a58
Dirhat Mohammad Salim <mdirhat@gmail.com>
2019-11-11 00:32:36 -0800

    SecurityConfig created

--- src/main/java/ch/zli/m223/punchclock/config/UserDetailsServiceImpl.java
+++ src/main/java/ch/zli/m223/punchclock/config/UserDetailsServiceImpl.java
@@ -1,4 +1,28 @@
 package ch.zli.m223.punchclock.config;
 
-public class UserDetailsServiceImpl {
+import ch.zli.m223.punchclock.repository.UserRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import static java.util.Collections.emptyList;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new User(user.getUsername(), user.getPassword(), emptyList());
+    }
 }
--- src/main/java/ch/zli/m223/punchclock/repository/UserRepository.java
+++ src/main/java/ch/zli/m223/punchclock/repository/UserRepository.java
@@ -4,4 +4,5 @@ import ch.zli.m223.punchclock.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    org.springframework.security.core.userdetails.User findByUsername(String username);
 }
