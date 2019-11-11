6ca8a3b69056e935d121b0df3cb2ddb9b61934dc
Dirhat Mohammad Salim <mdirhat@gmail.com>
2019-11-11 00:37:59 -0800

    Starting with the management pages.

--- src/main/java/ch/zli/m223/punchclock/config/WebSecurity.java
+++ src/main/java/ch/zli/m223/punchclock/config/WebSecurity.java
@@ -6,7 +6,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import com.auth0.samples.authapi.springbootauthupdated.user.UserDetailsServiceImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
